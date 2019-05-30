bc6e9c58104453e32a9e27606677a4116acead39
JennyLee <0608jennylee@hotmail.co.nz>
2019-05-30 13:38:14 +1200

    using replaceOne instead of replace one for database

--- src/main/java/ReviewerPersistence.java
+++ src/main/java/ReviewerPersistence.java
@@ -43,7 +43,7 @@ public class ReviewerPersistence {
 
     public void updateReviewCount(User codeReviewer){
         Document updatedUserCount = new Document(FIRST_NAME_KEY, codeReviewer.getName()).append(REVIEW_COUNT_KEY, codeReviewer.getReviewCount()).append(USERTYPE_KEY, UserType.NonDeveloper.ordinal());
-        mongoCollection.updateOne(eq(FIRST_NAME_KEY, codeReviewer.getName()), updatedUserCount);
+        mongoCollection.replaceOne(eq(FIRST_NAME_KEY, codeReviewer.getName()), updatedUserCount);
     }
 
     public int getReviewCountForUser(User codeReviewer){
